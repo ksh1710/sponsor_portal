f010bdc592247da034f2beb048e2cbb04cb5bc10
ksh1710 <kshitizagarwal1710@gmail.com>
2023-10-07 14:29:26 +0530

    saaf safai

---
+++ Frontend/sponsor_portal/src/Home.js
@@ -0,0 +1,12 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home_content">
+      <h1>Home</h1>
+      <p>This is the home page.</p>
+    </div>
+  );
+}
+
+export default Home;
